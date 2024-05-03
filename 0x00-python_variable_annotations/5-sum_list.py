c16916a2a2ac7dc62ab14025b299218a95848de8
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-05-03 19:39:38 +0000

    style

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 '''Complex types - list of floats'''
 
+
 from typing import List
 
 def sum_list(input_list: List[float]) -> float:
+    '''retun sum of the list'''
     return float(sum(input_list))
