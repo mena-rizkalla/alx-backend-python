e4132eecc24b4a1b08253395a628f96ed120b7a7
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-05-03 19:47:42 +0000

    fix import

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 '''Complex types - mixed list'''
 
-from type import List
+from typing import List
+
 
 def sum_mixed_list(mxd_lst: List[int]) -> float:
     return float(sum(mxd_lst))
