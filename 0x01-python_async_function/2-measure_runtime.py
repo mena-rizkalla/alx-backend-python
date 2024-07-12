8daceed7709b478c341c40701daf51bb4ea14ea5
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-07-13 02:44:37 +0300

    measure run time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Measure the runtime of a function
+"""
+
+import asyncio
+import random
+from time import perf_counter
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Return the elapsed time"""
+    s = pref_counter()
+    asyncio.run(wait_n(n, max_delay))
+    elapsed = pref_counter() - s
+    return elapsed
